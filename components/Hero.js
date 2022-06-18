e7c9dae3c7e5d6f6d0b5b80b32493dc9b9d6acea
akramrp <akramrp7@gmail.com>
2022-06-18 18:19:35 +0530

    change image location

--- components/Hero.js
+++ components/Hero.js
@@ -1,4 +1,4 @@
 import Image from 'next/image'
 export default function Hero() {
- return <Image src="/images/banner.jpg" width={1920} height={400} />
+ return <Image src="/../public/images/banner.jpg" alt="banner image" width={1920} height={400} />
 }
